db3688f52c3e9815dc776e275adc467dc10b0c5f
André <583546+oandregal@users.noreply.github.com>
2022-03-24 12:25:50 +0100

    Add transforms

--- packages/block-library/src/quote/v2/index.js
+++ packages/block-library/src/quote/v2/index.js
@@ -7,9 +7,10 @@ import { quote as icon } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
-import metadata from './block.json';
 import edit from './edit';
+import metadata from './block.json';
 import save from './save';
+import transforms from './transforms';
 
 const { name } = metadata;
 
@@ -30,6 +31,7 @@ const settings = {
 			},
 		],
 	},
+	transforms,
 	edit,
 	save,
 };
